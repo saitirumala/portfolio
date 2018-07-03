7013d83063151506f48566e87cacb1ff6ab0d3e0
sai tirumala <saiprasadtirumala.t@gmail.com>
2018-07-03 08:00:21 +0530

    added js file

---
+++ js/index.js
@@ -0,0 +1,19 @@
+console.log('hello we have js now');
+
+var x = 10;
+
+var name = 'sai';
+
+var isSaiMale = true;
+
+var human = {
+	color: 'white'
+};
+console.log(document.getElementById('Name'));
+document.getElementById('Name').style.color = 'red';
+
+var sai = human;
+
+console.log(sai);
+
+sai.hands = 2;
